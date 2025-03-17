f9ee9bddb8c7922bc18437f46bff6d24c43a0076
Bima Maulanam <bimawahyumaulana@gmail.com>
2025-03-17 21:29:18 +0700

    edit

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const port = 8000;
 
 app.use(
   cors({
-    origin: "*", // Mengizinkan semua origin (gunakan domain spesifik untuk produksi)
+    origin: ["http://localhost:3000"], //"*", // Mengizinkan semua origin (gunakan domain spesifik untuk produksi)
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
   })
