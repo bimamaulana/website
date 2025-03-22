794dc7bbdeaae26c262f09e3eec673fb2f265857
Bima Maulanam <bimawahyumaulana@gmail.com>
2025-03-22 08:14:49 +0700

    8080

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const moment = require("moment-timezone");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 app.use(
   cors({
