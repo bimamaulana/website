139014f5dbd2aea399d89a0070e35df2bf6c3e82
Bima Maulanam <bimawahyumaulana@gmail.com>
2025-03-25 11:40:01 +0700

    nambah folder styles 2

--- frontend/src/qr.js
+++ frontend/src/qr.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { QRCodeCanvas } from "qrcode.react";
-import "./QR.css";
+import "./styles/QR.css";
 
 const QR = () => {
   const getFormattedTime = () => {
