76f4f55692ba10b5f425c0e3673b71ff9db3b4ba
Bima Maulanam <bimawahyumaulana@gmail.com>
2025-03-25 11:20:35 +0700

    layout html qr 2

--- frontend/src/qr.js
+++ frontend/src/qr.js
@@ -40,7 +40,7 @@ const QR = () => {
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100 p-2">
-      <div className="text-center p-6 bg-white shadow-lg rounded-lg">
+      <div className="text-center p-6 bg-white shadow-lg rounded-lg w-full max-w-md">
         <h1 className="text-2xl font-bold">QR Code Generator</h1>
         <p className="my-4">QR Code akan diperbarui setiap menit</p>
         <div className="flex justify-center">
