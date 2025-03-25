d4c08238bd551350365ceaf5b8036cb6a1f04c1c
Bima Maulanam <bimawahyumaulana@gmail.com>
2025-03-25 10:49:03 +0700

    format qr

--- frontend/src/qr.js
+++ frontend/src/qr.js
@@ -4,9 +4,9 @@ import { QRCodeCanvas } from "qrcode.react";
 const QR = () => {
   const getFormattedTime = () =>
     new Date().toLocaleString(undefined, {
-      year: "numeric",
-      month: "2-digit",
       day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
       hour: "2-digit",
       minute: "2-digit",
       hour12: false,
