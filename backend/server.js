0c4c7ff614d82478fe12f9f175c423c9845a76c2
Bima Maulanam <bimawahyumaulana@gmail.com>
2025-03-28 09:23:41 +0700

    tambah db 3

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,6 @@ const db = mysql.createConnection({
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DBNAME,
-  //dbport: process.env.DB_PORT || 3306,
 });
 
 // Koneksi ke database
